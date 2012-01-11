0ae1e48e150ac251f6bd7420d542fa78b5d522bb
sectore <info@websector.de>
2012-01-11 14:50:49 +0100

    basic rails app structure without any specific content

---
+++ README.md
@@ -0,0 +1,18 @@
+#AngularJS + Rails port of Cafe Townsend application
+
+### Please note, this demo is still in progress...
+
+##About
+[AngularJS](http://http://angularjs.org/) and [Rails](http://rubyonrails.org/) port of the famous Cafe Townsend application originally written in ActionScript.
+
+##Screen shots
+coming soon
+
+##Live example
+coming soon
+
+##Local installation
+coming soon
+
+##Author
+Jens Krause // [WEBSECTOR.DE](http://www.websector.de)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CafeTownsendAngularRails::Application.config.secret_token = '7094a5e336ed6a515b7997e607189b0c1d2a11c7b833c2d7ca93ba2ec9dae6e159a8f1b5de4c546ac50700f63b22689335a52344e3d85c6f870eb8b5e6d6d255'
