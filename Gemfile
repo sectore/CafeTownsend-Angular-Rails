64e0ba2d92b138ef8016db1450d111a4c4c5ec8c
sectore <info@websector.de>
2012-01-17 13:40:50 +0100

    switching from uglifier to closure-compiler trying to avoid compressions issues on heroku

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'rails', '3.1.1'
 group :assets do
   gem 'sass-rails',   '~> 3.1.4'
   gem 'coffee-rails', '~> 3.1.1'
-  gem 'uglifier', '>= 1.0.3'
+  gem 'closure-compiler'
 end
 
 gem 'jquery-rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ CafeTownsendAngularRails::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = :closure
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
