d23971c4e8ad0bf1a8d66c81439850d3e634052f
Jens Krause <info@websector.de>
2013-04-07 11:02:42 +0200

    revert disabling of initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,5 @@ module CafeTownsendAngularRails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.assets.initialize_on_precompile = false
   end
 end
