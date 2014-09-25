16f80848f09d3a75b18c28056d2642f655799322
sectore <info@websector.de>
2014-09-25 13:31:41 +0200

    removing dependencies of spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,47 +1,40 @@
 require 'rubygems'
-require 'spork'
 require 'database_cleaner'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
 
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
 
-  RSpec.configure do |config|
-    config.use_transactional_fixtures = false
-    config.infer_base_class_for_anonymous_controllers = false
-    config.order = "random"
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-    config.before :each do
-     if Capybara.current_driver == :selenium
-       DatabaseCleaner.strategy = :truncation
-     else
-       DatabaseCleaner.strategy = :transaction
-     end
-     DatabaseCleaner.start
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+
+  config.before :each do
+    if Capybara.current_driver == :selenium
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
     end
-
-    config.after(:each) do
-     DatabaseCleaner.clean
-    end
-
-    # Include Factory Girl syntax to simplify calls to factories
-    config.include FactoryGirl::Syntax::Methods
-
-    config.include ControllerMacros
+    DatabaseCleaner.start
   end
-end
 
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  FactoryGirl.reload
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  # Include Factory Girl syntax to simplify calls to factories
+  config.include FactoryGirl::Syntax::Methods
+
+  config.include ControllerMacros
 end
 
 # Faker does not support random dates
 # https://github.com/stympy/faker/pull/73
-# So lets extends Faker
+# So lets extend Faker
 # https://gist.github.com/rjackson/4694263
 class Faker::Date
   def self.random
